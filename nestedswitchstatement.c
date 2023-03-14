a26c9d7cbdf1ca1cfb93dd516bac0ffa94ee2b12
SatyamRattan <124145531+SatyamRattan@users.noreply.github.com>
2023-03-14 10:04:39 +0530

    Add files via upload

---
+++ nestedswitchstatement.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i=10;
+	int j=20;
+	
+	switch(i)
+	{
+		case 10:
+			printf("the value of i evaluated in outer switch:%d\n",i);
+		case 20:
+			switch(j)
+			{
+				case 20:
+					printf("the value ")
+			}
+	}
+}
